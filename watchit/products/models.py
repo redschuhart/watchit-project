99ea70f235cd7cc76ac89a9a6f7bccf77340869c
redschuhart <notbeatifulsoup@gmail.com>
2023-11-30 17:42:39 +0600

    For product model added short_description field Fixed wrong product name displaying in product card Nav bar changed to sticky Templates moved to project core Now base.html uses in both products and watch store apps instead 2 same templates Fixed styles import in base.html Tempaltes folders removed from apps In settings.py added new template dir

--- watchit/products/models.py
+++ watchit/products/models.py
@@ -5,8 +5,10 @@ from django.core.validators import MaxValueValidator
 
 class ProductCategory(models.Model):
     category_id = models.AutoField(primary_key=True)
-    category_name = models.CharField(blank=False, null=False, max_length=255)
-    category_description = models.TextField(blank=None, null=None)
+    category_name = models.CharField(blank=False, null=False, max_length=255,
+                                     verbose_name='Название категории')
+    category_description = models.TextField(blank=None, null=None,
+                                            verbose_name='Описание категории')
 
     def __str__(self):
         return f'{self.category_name}'
@@ -14,8 +16,11 @@ class ProductCategory(models.Model):
 
 class ProductBrand(models.Model):
     brand_id = models.AutoField(primary_key=True)
-    brand_name = models.CharField(blank=False, null=False, max_length=255)
-    brand_description = models.TextField(blank=None, null=None)
+    brand_name = models.CharField(blank=False, null=False, max_length=255,
+                                  verbose_name='Название бренда')
+    brand_description = models.TextField(blank=None, null=None,
+                                         verbose_name='Описание бренда')
+
 
     def __str__(self):
         return f"{self.brand_name}"
@@ -23,7 +28,8 @@ class ProductBrand(models.Model):
 
 class ProductType(models.Model):
     type_id = models.AutoField(primary_key=True)
-    type_name = models.CharField(blank=False, null=False, max_length=255)
+    type_name = models.CharField(blank=False, null=False, max_length=255,
+                                 verbose_name='Тип часов')
 
     def __str__(self):
         return f"{self.type_name}"
@@ -31,29 +37,69 @@ class ProductType(models.Model):
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255, unique=True, blank=False, null=False)
-    product_name = models.CharField(blank=False, null=False, max_length=255, unique=True)
-    product_slug = models.CharField(max_length=255, null=False, blank=False)
+    product_name = models.CharField(blank=False, null=False, max_length=255, unique=True,
+                                    verbose_name='Название'
+                                    )
+    product_slug = models.CharField(max_length=255, null=False, blank=False,
+                                    verbose_name='Модель товара'
+                                    )
     time_create = models.TimeField(auto_now_add=True)
     time_update = models.TimeField(auto_now_add=True)
-    quantity = models.PositiveIntegerField(blank=False, null=False, default=0)
-    discount = models.PositiveIntegerField(blank=False, null=False, default=0, validators=[MaxValueValidator(100)])
-    price = models.DecimalField(blank=False, null=False, max_digits=7, decimal_places=2)
-    description = models.TextField(blank=False, null=False)
-    main_image = models.ImageField(upload_to='products_images')
-    made_in = models.CharField(blank=False, null=False, max_length=255)
-    movement = models.CharField(blank=False, null=False, max_length=255)
-    category_id = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
-    case_material = models.CharField(blank=False, null=False, max_length=255)
-    color = models.CharField(blank=False, null=False, max_length=255)
-    brand_id = models.ForeignKey(to=ProductBrand, on_delete=models.CASCADE)
-    water_resistance = models.IntegerField(blank=False, null=False, validators=[MaxValueValidator(500)])
-    backlight = models.BooleanField(blank=False, null=False)
-    backlight_type = models.CharField(blank=False, null=False, max_length=255)
-    type_id = models.ForeignKey(to=ProductType, on_delete=models.CASCADE)
-    glass = models.CharField(blank=False, null=False, max_length=255)
-    calendar = models.CharField(max_length=255, default=False)
-    size = models.CharField(blank=False, null=False, max_length=255)
-    sex = models.CharField(blank=False, null=False, max_length=255, default='Мужские')
+    quantity = models.PositiveIntegerField(blank=False, null=False, default=0,
+                                           verbose_name='Колличество на складе'
+                                           )
+    short_description = models.TextField(blank=False, null=False,
+                                         verbose_name='Краткое описание'
+                                         )
+
+    discount = models.PositiveIntegerField(blank=False, null=False, default=0,
+                                           validators=[MaxValueValidator(50, message='Скидка не может быть больше 100%')],
+                                           verbose_name='Размер скидки в %'
+    )
+    price = models.DecimalField(blank=False, null=False, max_digits=7,
+                                decimal_places=2, verbose_name='Цена'
+                                )
+    description = models.TextField(blank=False, null=False, verbose_name='Описание товара')
+    main_image = models.ImageField(upload_to='products_images', verbose_name='Фото'
+                                   )
+    made_in = models.CharField(blank=False, null=False, max_length=255,
+                               verbose_name='Страна производства'
+                               )
+    movement = models.CharField(blank=False, null=False, max_length=255,
+                                verbose_name='Механизм')
+    category_id = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE,
+                                    verbose_name='Категория')
+    case_material = models.CharField(blank=False, null=False, max_length=255,
+                                     verbose_name='Материал корпуса'
+                                     )
+    color = models.CharField(blank=False, null=False, max_length=255,
+                             verbose_name='Цвет'
+                             )
+    brand_id = models.ForeignKey(to=ProductBrand, on_delete=models.CASCADE,
+                                 verbose_name='Название бренда'
+                                 )
+    water_resistance = models.IntegerField(blank=False, null=False,
+                                           validators=[MaxValueValidator(500)],
+                                           verbose_name='Водозащита в метрах')
+    backlight = models.BooleanField(blank=False, null=False,
+                                    verbose_name='Подсветка')
+    backlight_type = models.CharField(blank=False, null=False, max_length=255,
+                                      verbose_name='Тип подсветки'
+                                      )
+    type_id = models.ForeignKey(to=ProductType, on_delete=models.CASCADE,
+                                verbose_name='Тип часов'
+                                )
+    glass = models.CharField(blank=False, null=False, max_length=255,
+                             verbose_name='Стекло'
+                             )
+    calendar = models.CharField(max_length=255, default=False,
+                                verbose_name='Календарь'
+                                )
+    size = models.CharField(blank=False, null=False, max_length=255,
+                            verbose_name='Размеры')
+    sex = models.CharField(blank=False, null=False, max_length=255,
+                           verbose_name='Мужские/женские'
+                           )
 
     def __str__(self):
         return f'{self.product_name}'
--- watchit/watchit/settings.py
+++ watchit/watchit/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'watchit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,6 +130,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
