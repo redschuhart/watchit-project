54786e977ba091a486b45139cea642c8e5d0a72e
redschuhart <notbeatifulsoup@gmail.com>
2023-12-19 19:22:59 +0600

    Reworked signin view Deleted sucess/fail page for auth Changed header icon sizes and font size Removed usless css Added error message for failed signin

--- read.md
+++ read.md
@@ -7,11 +7,11 @@
 * from django.core.management.utils import get_random_secret_key
 * print(get_random_secret_key())
 * SECRET_KEY='GENERATED SECRET KEY'  
-* 
+
 # 3. Make migrations
 * ./manage.py makemigrations
 * ./manage.py migrate  
-* 
+
 # 4. Load fixtures
 * ./manage.py loaddata fixtures/products/brands.json(Load all fixtures from directory)
  
--- watchit/users/views.py
+++ watchit/users/views.py
@@ -15,18 +15,18 @@ def register(request):
         return render(request, template_name='users/register.html')
 
 
-# TODO post data verification
+
 def signin(request):
+    context = {
+        'form': SigninForm()
+    }
     if request.method == 'POST':
         post_data = request.POST
         user = auth.authenticate(username=post_data['username'], password=post_data['password'])
         if user:
             auth.login(request, user)
-            return render(request, template_name='users/success_signin.html')
-        return render(request, template_name='users/fail_signin.html')
-    else:
-        context = {
-            'form': SigninForm()
-        }
-        print(request.user.is_authenticated)
-        return render(request, template_name='users/signin.html', context=context)
+            return redirect(to='home')
+        context['wrong_data'] = 'Не правильный логин или пароль'
+
+
+    return render(request, template_name='users/signin.html', context=context)
